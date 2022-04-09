2b90d0d9b8daae0c4671e59c626bd6c89c2cea7f
MrDan4es <mrdan4es@gmail.com>
2022-04-09 15:29:36 +0500

    old code deleted

--- storage/views.py
+++ storage/views.py
@@ -22,31 +22,3 @@ class ImageViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
         
         return Response({'image': serializer.data})
-
-# class ImageAPIList(generics.ListCreateAPIView):
-#     serializer_class = ImageSerializer
-    
-#     def get_queryset(self):
-#         return Image.objects.filter(user=self.request.user)
-
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         if not serializer.is_valid():
-#             print(request.data)
-#             print(serializer.errors)
-            
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(user=self.request.user)
-        
-#         return Response({'image': serializer.data})
-    
-    
-# class ImageAPIDelete(generics.DestroyAPIView):
-#     serializer_class = ImageSerializer
-    
-#     def get_queryset(self):
-#         return Image.objects.filter(user=self.request.user)
-    
-# class ImageView(ModelViewSet):
-#     queryset = Image.objects.all()
-#     serializer_class = ImageSerializer
